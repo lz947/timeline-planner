618cee8c2b3b5ad5a227388dee1007ac2625f457
lz947 <zy.liu947@gmail.com>
2025-04-03 21:22:16 -0400

    Create the global state

---
+++ utils/ProjectState.tsx
@@ -0,0 +1,91 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+
+//entities
+// {id: {id:xxx, name:xxx...}}
+interface Entity {
+  id: string;
+  name: string;
+}
+
+interface projectState {
+  projectProjectName: string;
+  entities: Record<string, Entity>;
+}
+
+interface ProjectStateContextType {
+  state: projectState;
+  setProjectName: (newProjectProjectName: string) => void;
+  addEntity: (id: string, newEntity: Entity) => void;
+  editEntity: (id: string, newEntity: Entity) => void;
+  deleteEntity: (id: string) => void;
+}
+
+// Define the context
+const StateContext = createContext<ProjectStateContextType | undefined>(undefined);
+
+// Provider component
+export const StateProvider = ({ children } : { children:any }) => {
+  const [state, setState] = useState<projectState>({
+    projectProjectName: "MyApp",
+    entities: {},
+  });
+
+  const setProjectName = (newProjectProjectName: string) => {
+    setState((prevState) => ({
+      ...prevState,
+      projectProjectName: newProjectProjectName,
+    }));
+  };
+
+  // Add a new user
+  const addEntity = (id: string, newEntity: Entity) => {
+    setState((prevState) => ({
+      ...prevState,
+      entities: {
+        ...prevState.entities,
+        [id]: newEntity,
+      },
+    }));
+  };
+
+  // Edit a user
+  const editEntity = (id: string, newEntity: Entity) => {
+    setState((prevState) => ({
+      ...prevState,
+      entities: {
+        ...prevState.entities,
+        [id]: newEntity,
+      },
+    }));
+  };
+
+  // Delete a user
+  const deleteEntity = (id: string) => {
+    setState((prevState) => {
+      const newEntities = { ...prevState.entities };
+      delete newEntities[id];
+
+      return { ...prevState, entities: newEntities };
+    });
+  };
+
+  return (
+    <StateContext.Provider 
+      value={{ 
+        state, setProjectName, 
+        addEntity, editEntity, deleteEntity  
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+// Custom hook to use the state
+export const useProjectpState = (): ProjectStateContextType => {
+  const context = useContext(StateContext);
+  if (!context) {
+    throw new Error("useProjectpState must be used within a StateProvider");
+  }
+  return context;
+};
