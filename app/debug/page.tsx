822f1ea29c30a6b0cdecf9de70c030ac0991f711
chenweihang2021 <chenweihang2021@outlook.com>
2025-04-13 19:32:06 -0700

    add basic page for debug

---
+++ app/debug/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useProjectState } from "@/utils/ProjectState";
+
+const Debug = () => {
+  const { projectState } = useProjectState();
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div>
+        Entities
+      </div>
+      <div>
+        {JSON.stringify(projectState, null, 2)}
+      </div>
+    </section>
+  );
+}
+
+export default Debug;
+  
